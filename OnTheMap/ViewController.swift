8ccb7ee0aabdb92c5fcbd915f4c7aa6026e08f6f
stephen bai <stephenbai91@gmail.com>
2017-08-24 18:16:04 -0400

    first commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by stephen on 8/24/17.
+//  Copyright © 2017 Bai Cloud Tech Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
